2c4b3f224ab853c3b2f9a35bb4d3bf8aeb80c5c7
williamsuane <william.suane@devdojo.academy>
2020-05-07 20:28:06 +0200

    Project Reactor Essentials 01 - Project Setup

---
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package academy.devdojo.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test(){
+        log.info("Everything working as intended");
+    }
+}
