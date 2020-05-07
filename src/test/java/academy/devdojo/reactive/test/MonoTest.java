aa79a6e9826fa3054db24fed433007e38565fd70
williamsuane <william.suane@devdojo.academy>
2020-05-07 21:42:03 +0200

    Project Reactor Essentials 03 - Subscribing to Mono

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -2,27 +2,37 @@ package academy.devdojo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
-/**
- * Reactive Streams
- * 1. Asynchronous
- * 2. Non-blocking
- * 3. Backpressure
- * Publisher <- (subscribe) Subscriber
- * Subscription is created
- * Publisher (onSubscribe with the subscription) -> Subscriber
- * Subscription <- (request N) Subscriber
- * Publisher -> (onNext) Subscriber
- * until:
- * 1. Publisher sends all the objects requested.
- * 2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
- * 3. There is an error. (onError) -> subscriber and subscription will be canceled
+/*
+  Reactive Streams
+  1. Asynchronous
+  2. Non-blocking
+  3. Backpressure
+  Publisher <- (subscribe) Subscriber
+  Subscription is created
+  Publisher (onSubscribe with the subscription) -> Subscriber
+  Subscription <- (request N) Subscriber
+  Publisher -> (onNext) Subscriber
+  until:
+  1. Publisher sends all the objects requested.
+  2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
+  3. There is an error. (onError) -> subscriber and subscription will be canceled
  */
 public class MonoTest {
 
     @Test
-    public void test(){
-        log.info("Everything working as intended");
+    public void monoSubscriber() {
+        String name = "William Suane";
+        Mono<String> mono = Mono.just(name)
+            .log();
+
+        mono.subscribe();
+        log.info("--------------------------");
+        StepVerifier.create(mono)
+            .expectNext(name)
+            .verifyComplete();
     }
 }
