e0456cf7fade3ef816cc1439145daebf46c3e408
williamsuane <william.suane@devdojo.academy>
2020-05-07 20:46:00 +0200

    Project Reactor Essentials 02 - Quick recap reactive streams

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -4,6 +4,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
+/**
+ * Reactive Streams
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subscribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * Publisher -> (onNext) Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested.
+ * 2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
+ * 3. There is an error. (onError) -> subscriber and subscription will be canceled
+ */
 public class MonoTest {
 
     @Test
