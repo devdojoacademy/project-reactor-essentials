c212a3e8ae3c20bae378dc3b201301aa00ee4e50
williamsuane <william.suane@devdojo.academy>
2020-05-08 20:15:59 +0200

    Project Reactor Essentials 05 - Subscribing to Mono with Consumer, Error, Complete and Subscription

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -2,6 +2,7 @@ package academy.devdojo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -54,7 +55,9 @@ public class MonoTest {
     public void monoSubscriberConsumerError() {
         String name = "William Suane";
         Mono<String> mono = Mono.just(name)
-            .map(s -> {throw new RuntimeException("Testing mono with error");});
+            .map(s -> {
+                throw new RuntimeException("Testing mono with error");
+            });
 
         mono.subscribe(s -> log.info("Name {}", s), s -> log.error("Something bad happened"));
         mono.subscribe(s -> log.info("Name {}", s), Throwable::printStackTrace);
@@ -64,5 +67,42 @@ public class MonoTest {
         StepVerifier.create(mono)
             .expectError(RuntimeException.class)
             .verify();
+    }
+
+    @Test
+    public void monoSubscriberConsumerComplete() {
+        String name = "William Suane";
+        Mono<String> mono = Mono.just(name)
+            .log()
+            .map(String::toUpperCase);
+
+        mono.subscribe(s -> log.info("Value {}", s),
+            Throwable::printStackTrace,
+            () -> log.info("FINISHED!"));
+
+        log.info("--------------------------");
+
+        StepVerifier.create(mono)
+            .expectNext(name.toUpperCase())
+            .verifyComplete();
+    }
+
+    @Test
+    public void monoSubscriberConsumerSubscription() {
+        String name = "William Suane";
+        Mono<String> mono = Mono.just(name)
+            .log()
+            .map(String::toUpperCase);
+
+        mono.subscribe(s -> log.info("Value {}", s),
+            Throwable::printStackTrace,
+            () -> log.info("FINISHED!")
+            , Subscription::cancel);
+
+        log.info("--------------------------");
+
+        StepVerifier.create(mono)
+            .expectNext(name.toUpperCase())
+            .verifyComplete();
     }
 }
