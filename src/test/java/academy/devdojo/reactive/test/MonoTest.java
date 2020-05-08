392e2f10599232101e451fe6c8b7f5308634ec28
williamsuane <william.suane@devdojo.academy>
2020-05-08 21:25:06 +0200

    Project Reactor Essentials 06 - Mono doOnSubscribe, doOnRequest, doOnNext, doOnSuccess

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -97,12 +97,36 @@ public class MonoTest {
         mono.subscribe(s -> log.info("Value {}", s),
             Throwable::printStackTrace,
             () -> log.info("FINISHED!")
-            , Subscription::cancel);
+            , subscription ->  subscription.request(5));
 
         log.info("--------------------------");
 
         StepVerifier.create(mono)
             .expectNext(name.toUpperCase())
             .verifyComplete();
+    }
+
+    @Test
+    public void monoDoOnMethods() {
+        String name = "William Suane";
+        Mono<Object> mono = Mono.just(name)
+            .log()
+            .map(String::toUpperCase)
+            .doOnSubscribe(subscription -> log.info("Subscribed"))
+            .doOnRequest(longNumber -> log.info("Request Received, starting doing something..."))
+            .doOnNext(s -> log.info("Value is here. Executing doOnNext {}",s))
+            .flatMap(s -> Mono.empty())
+            .doOnNext(s -> log.info("Value is here. Executing doOnNext {}",s)) //will not be executed
+            .doOnSuccess(s -> log.info("doOnSuccess executed {}", s));
+
+        mono.subscribe(s -> log.info("Value {}", s),
+            Throwable::printStackTrace,
+            () -> log.info("FINISHED!"));
+
+        log.info("--------------------------");
+
+//        StepVerifier.create(mono)
+//            .expectNext(name.toUpperCase())
+//            .verifyComplete();
     }
 }
