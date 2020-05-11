d8b0663938cbaa177a1ea43fe0ca9109888fd5cd
williamsuane <william.suane@devdojo.academy>
2020-05-11 19:53:42 +0200

    Project Reactor Essentials 12 - Backpressure with limitRate

--- src/test/java/academy/devdojo/reactive/test/FluxTest.java
+++ src/test/java/academy/devdojo/reactive/test/FluxTest.java
@@ -146,6 +146,20 @@ public class FluxTest {
     }
 
     @Test
+    public void fluxSubscriberPrettyBackpressure() {
+        Flux<Integer> flux = Flux.range(1, 10)
+            .log()
+            .limitRate(3);
+
+        flux.subscribe(i -> log.info("Number {}", i));
+
+        log.info("-----------------------------------");
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            .verifyComplete();
+    }
+
+    @Test
     public void fluxSubscriberIntervalOne() throws Exception {
         Flux<Long> interval = Flux.interval(Duration.ofMillis(100))
             .take(10)
