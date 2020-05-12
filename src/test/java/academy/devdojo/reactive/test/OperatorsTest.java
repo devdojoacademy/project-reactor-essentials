437e3a2ce7a6ed1efc1d8478b521191e08e86217
williamsuane <william.suane@devdojo.academy>
2020-05-12 21:14:29 +0200

    Project Reactor Essentials 17 - fromCallable

--- src/test/java/academy/devdojo/reactive/test/OperatorsTest.java
+++ src/test/java/academy/devdojo/reactive/test/OperatorsTest.java
@@ -1,8 +1,13 @@
 package academy.devdojo.reactive.test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
@@ -125,5 +130,24 @@ public class OperatorsTest {
             .expectSubscription()
             .expectNext(1, 2, 3, 4)
             .verifyComplete();
+    }
+
+    @Test
+    public void subscribeOnIO() throws Exception{
+        Mono<List<String>> list = Mono.fromCallable(() -> Files.readAllLines(Path.of("text-file")))
+            .log()
+            .subscribeOn(Schedulers.boundedElastic());
+
+//        list.subscribe(s -> log.info("{}",s));
+
+        StepVerifier.create(list)
+        .expectSubscription()
+        .thenConsumeWhile(l -> {
+            Assertions.assertFalse(l.isEmpty());
+            log.info("Size {}",l.size());
+            return true;
+        })
+        .verifyComplete();
+
     }
 }
